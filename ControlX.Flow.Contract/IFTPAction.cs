4d5b58537297f8dbf2ea5bd087ecc755f28a9304
Erik Rosemann <erik.rosemann@online.de>
2021-11-01 22:56:48 +0100

    add ftp download, prepare appinsights monitor

--- ControlX.Flow.Contract/IFTPAction.cs
+++ ControlX.Flow.Contract/IFTPAction.cs
@@ -5,6 +5,18 @@ public interface IFTPAction : IAction
     public int Port { get; set; }
     public string Path { get; set; }
     public string UserName { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
+    public string? PasswordFile { get; set; }
+    public string? PrivateKeyFile { get; set; }
+    public string? PassPhrase { get; set; }
+    public string? FingerPrint { get; set; }
     public string SourceFile { get; set; }
+    public string? DownloadDestination { get; set; }
+    public FTPMethod FTPMethod {  get; set; }
+}
+
+public enum FTPMethod
+{
+    Upload = 0,
+    Download = 1
 }
--- ControlX.Flow/FTPAction.cs
+++ ControlX.Flow/FTPAction.cs
@@ -2,22 +2,29 @@
 using Renci.SshNet;
 using Dahomey.Json.Attributes;
 using Microsoft.Extensions.Logging;
+using Renci.SshNet.Common;
 
 namespace ControlX.Flow.Core;
 
 [JsonDiscriminator(nameof(FTPAction))]
 public class FTPAction : FlowAction<FTPAction>, IFTPAction
 {
+
+    #region Properties
     public string Host {  get; set; }
     public int Port { get; set; }
     public string Path { get; set; }
     public string UserName { get; set; }
     public string? Password { get; set; }
+    public string? PasswordFile { get; set; }
     public string? PrivateKeyFile { get; set; }
     public string? PassPhrase { get; set; }
     public string? FingerPrint { get; set; }
-    public string SourceFile { get; set; }
+    public string? SourceFile { get; set; }
+    public string? DownloadDestination { get; set; }
     public IAutomate Automate { get; set; }
+    public FTPMethod FTPMethod { get; set; } = FTPMethod.Upload;
+    #endregion
 
     public async Task RunAsync()
     {
@@ -25,7 +32,11 @@ public class FTPAction : FlowAction<FTPAction>, IFTPAction
 
         using (_logger.BeginScope(this))
         {
+            #region authentification
             var authentificationMethods = new List<AuthenticationMethod>();
+
+            if(PasswordFile != null)
+                Password = File.ReadAllText(PasswordFile);
 
             if (Password != null)
                 authentificationMethods.Add(
@@ -47,31 +58,66 @@ public class FTPAction : FlowAction<FTPAction>, IFTPAction
                 UserName,
                 authentificationMethods.ToArray()
             );
+            #endregion
 
             using (var client = new SftpClient(connectionInfo))
             {
-                client.HostKeyReceived += (sender, e) =>
-                {
-                    if (!string.IsNullOrWhiteSpace(FingerPrint))
-                    {
-                        if (BitConverter.ToString(e.FingerPrint).Replace('-', ':') == FingerPrint)
-                            e.CanTrust = true;
-                        else
-                            e.CanTrust = false;
-                    }
-                    else
-                        e.CanTrust = true;
-                };
+                // check fingerprint
+                client.HostKeyReceived += HandleHostKeyReceived;
 
-                using (var file = new FileStream(SourceFile, FileMode.Open))
+                switch(FTPMethod)
                 {
-                    var fileName = SourceFile.Split('\\').Last();
-                    client.Connect();
-                    client.UploadFile(file, Path + fileName, null);
+                    case FTPMethod.Upload:
+                        UploadFile(client);
+                        break;
+                    case FTPMethod.Download:
+                        DownloadFile(client);
+                        break;
                 }
             }
 
-            _logger.LogInformation($"FTP-Action: File {SourceFile} transferd to {Host} path {Path}");
+            
         }   
+    }
+
+    private void HandleHostKeyReceived(object? sender, HostKeyEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(FingerPrint))
+        {
+            if (BitConverter.ToString(e.FingerPrint).Replace('-', ':') == FingerPrint)
+                e.CanTrust = true;
+            else
+                e.CanTrust = false;
+        }
+        else
+            e.CanTrust = true;
+    }
+
+    private void UploadFile(SftpClient client)
+    {
+        using (var file = new FileStream(SourceFile, FileMode.Open))
+        {
+            var fileName = SourceFile.Split('\\').Last();
+            client.Connect();
+            client.UploadFile(file, Path + fileName, null);
+        }
+
+        _logger.LogInformation($"FTP-Action: File {SourceFile} uploaded to {Host} path {Path}");
+    }
+
+    private void DownloadFile(SftpClient client)
+    {
+        var fileName = Path.Split('/').Last();
+        var destinationPath = System.IO.Path.Combine(DownloadDestination, fileName);
+
+        using (var file = new FileStream(destinationPath, FileMode.CreateNew))
+        {
+            
+            client.Connect();
+            client.DownloadFile(Path, file);
+        }
+
+        _logger.LogInformation($"FTP-Action: File {Path} downloaded from {Host} to local path {DownloadDestination}");
+
     }
 }
